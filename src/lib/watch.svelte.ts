69f7f0d5388ee9b9be7d5fddd914e4e442e79a16
Dinh-Van Colomban <van.colomban@gmail.com>
2025-02-05 02:27:58 +0100

    feat(watch): adds skip condition

--- src/lib/watch.svelte.ts
+++ src/lib/watch.svelte.ts
@@ -1,14 +1,29 @@
 import { untrack } from 'svelte';
 
-export function watch(sources: () => unknown, untracked: Parameters<typeof untrack>[0]) {
+export type WatchOptions = { skipFirst?: boolean; skip?: () => boolean };
+export function watch(sources: () => unknown, untracked: Parameters<typeof untrack>[0], options: WatchOptions = {}) {
+  let first = true;
+  const skip = ({ skipFirst }: WatchOptions = {}) => {
+    if (!first) return options?.skip?.();
+    first = false;
+    return skipFirst || options?.skip?.();
+  };
   $effect(() => {
+    if (skip(options)) return;
     sources();
     untrack(untracked);
   });
 }
 
-export function effect(sources: () => unknown, tracked: () => void) {
+export function effect(sources: () => unknown, tracked: () => void, options: WatchOptions = {}) {
+  let first = true;
+  const skip = ({ skipFirst }: WatchOptions = {}) => {
+    if (!first) return options?.skip?.();
+    first = false;
+    return skipFirst || options?.skip?.();
+  };
   $effect(() => {
+    if (skip(options)) return;
     sources();
     tracked();
   });
